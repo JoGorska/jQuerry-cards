06fdba3123b5c73959e206565101b7c444463ca9
Joanna Gorska <gorska919@gmail.com>
2021-10-12 21:07:16 +0000

    Add example of events and animations

--- script.js
+++ script.js
@@ -2,23 +2,35 @@ $(document).ready(function() {
  
 
     $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-       $(".stream2").removeClass('highlight_stream');
-       $(".stream3").removeClass('highlight_stream');
-         $(".stream1").addClass('highlight_stream');
+        $(".stream1").show('');
+       $(".stream2").hide('slow');
+       $(".stream3").show('fast');
+         $(".stream1").hide('1000');
    });
    $("#stream2_btn").on("click", function() {
-       $(".stream1").removeClass('highlight_stream');
-       $(".stream2").removeClass('highlight_stream');
+       $(".stream1").toggle();
+       $(".stream2").slideDown();
        $(".stream3").removeClass('highlight_stream');
          $(".stream2").addClass('highlight_stream');
    });
    $("#stream3_btn").on("click", function() {
-       $(".stream1").removeClass('highlight_stream');
+       $(".stream1").slideDown();
        $(".stream2").removeClass('highlight_stream');
        $(".stream3").removeClass('highlight_stream');
          $(".stream3").addClass('highlight_stream');
    });
+   $(".stream1").click(function(){
+      $(".stream1").css("background-color", "green")
+   });
+   $(".stream2").mouseenter(function(){
+     $(".stream2").css("background-color", "pink")
+   })
 
 
 }); 
+
+
+// other events
+// mouse enter
+// mouse leave
+// click
